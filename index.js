2ada0f97c72dc3669c51953467f7ddeea0098034
Fayis <fayizpachu217@gmail.com>
2026-04-26 16:50:37 +0530

    skill capcut added done

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ import socketHandler from "./socket/socket.js";
 import connectDB from "./config/db.js";
 
 const app = express();
-app.use(cors({ origin: ["https://teambcz.netlify.app","http://localhost:5173"], credentials: true }));
+app.use(cors({ 
+  origin: ["https://teambcz.netlify.app", "http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:5174", "http://127.0.0.1:5174"], 
+  credentials: true 
+}));
 
 app.use(express.json());
 
